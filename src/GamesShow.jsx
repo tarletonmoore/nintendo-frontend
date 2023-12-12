39f07059c41c1aab5ef60a561ddab9f46705e978
Tarleton Moore <tarletonmoore@Tarletons-MacBook-Pro.local>
2023-12-12 14:18:08 -0500

    add review and delete review on game show page

--- src/GamesShow.jsx
+++ src/GamesShow.jsx
@@ -32,16 +32,56 @@ export function GamesShow(props) {
     event.preventDefault()
     const params = new FormData(event.target);
 
-    console.log('adding to cart')
     axios.post("http://localhost:3000/favorites.json", params).then(response => {
       console.log(response.data)
       window.location.href = '/me'
     })
   }
 
+  const handleAddReview = (event) => {
+    event.preventDefault()
+    const params = new FormData(event.target);
+
+    axios.post("http://localhost:3000/reviews.json", params).then((response) => {
+      const newReview = {
+        id: response.data.id,
+        review: params.get("review"),
+        user: {
+          id: props.currentUser.id,
+          name: props.currentUser.name,
+        },
+      };
+
+      setGame((prevGame) => ({
+        ...prevGame,
+        reviews: [...prevGame.reviews, newReview],
+      }));
+      event.target.elements.review.value = "";
+    })
+  }
+
   const checkIfFavorited = () => {
     const found = props.currentUser.favorites.some(fav => fav.game.id === game.id);
     setIsFavorited(found);
+  };
+
+  const handleDestroyReview = (review) => {
+    axios.delete(`http://localhost:3000/reviews/${review.id}.json`)
+      .then((response) => {
+
+        setGame((prevGame) => ({
+          ...prevGame,
+          reviews: prevGame.reviews.filter((r) => r.id !== review.id),
+        }));
+      })
+      .catch((error) => {
+        console.error('Error deleting review:', error);
+      });
+  };
+
+
+  const handleClick = (review) => {
+    handleDestroyReview(review);
   };
 
   useEffect(() => {
@@ -63,7 +103,7 @@ export function GamesShow(props) {
       <div className="card-body">
         <h1>{game.title}</h1>
         <img src={game.image} height="250px" />
-        <p>Price: {game.price}</p>
+        <p>Price: ${game.price}</p>
         <p>Console: {game.console.name}</p>
         <form onSubmit={handleAddToCart}>
           <div>
@@ -75,7 +115,7 @@ export function GamesShow(props) {
           <br></br>
           <button>Add to cart</button>
         </form>
-
+        <br></br>
         {isFavorited ? (
           <p>This game is already in your wishlist</p>
         ) : (
@@ -92,11 +132,30 @@ export function GamesShow(props) {
         )}
         <br></br>
         <h2>Reviews:</h2>
+        <form onSubmit={handleAddReview}>
+          <div>
+            <input name="game_id" type="hidden" defaultValue={game.id} />
+          </div>
+          <div>
+            <input name="user_id" type="hidden" defaultValue={props.currentUser.id} />
+          </div>
+          <div>
+            <input name="review" type="text" defaultValue={game.review} />
+          </div>
+          <br />
+          <button>Add review</button>
+        </form>
+        <br></br>
         {game.reviews.length > 0 ? (
           game.reviews.map(review => (
             <div key={review.id} className="card">
               <div className="card-body">
                 <p>{review.user.name}: {review.review}</p>
+                {review.user.id === props.currentUser.id && (
+                  <>
+                    <button onClick={() => handleClick(review)}>Delete Review</button>
+                  </>
+                )}
               </div>
             </div>
           ))

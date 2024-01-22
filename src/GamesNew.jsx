0894d17d8b38678aab66b8376d85f1c9a8bd8c0d
Tarleton Moore <tarletonmoore@gmail.com>
2024-01-22 10:14:04 -0500

    styling

--- src/GamesNew.jsx
+++ src/GamesNew.jsx
@@ -14,44 +14,45 @@ export function GamesNew(props) {
   return (
     <div>
       <h1>New Game</h1>
-      <Card className="console">
-        <Card.Body>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group className='mb-3'>
-              <Form.Label>Title:</Form.Label>
-              <Form.Control type="text" name="title" placeholder="Enter game title" style={{ width: '300px' }} />
-            </Form.Group>
+      <div className="d-flex justify-content-center align-items-center">
+        <Card className="console" >
+          <Card.Body>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group className='mb-3'>
+                <Form.Label>Title:</Form.Label>
+                <Form.Control type="text" name="title" placeholder="Enter game title" style={{ width: '300px' }} />
+              </Form.Group>
 
-            <Form.Group className='mb-3'>
-              <Form.Label>Image:</Form.Label>
-              <Form.Control type="text" name="image" placeholder="Enter game image URL" style={{ width: '300px' }} />
-            </Form.Group>
+              <Form.Group className='mb-3'>
+                <Form.Label>Image:</Form.Label>
+                <Form.Control type="text" name="image" placeholder="Enter game image URL" style={{ width: '300px' }} />
+              </Form.Group>
 
-            <Form.Group className='mb-3'>
-              <Form.Label>Price:</Form.Label>
-              <Form.Control type="number" name="price" placeholder="Enter game price" style={{ width: '300px' }} />
-            </Form.Group>
+              <Form.Group className='mb-3'>
+                <Form.Label>Price:</Form.Label>
+                <Form.Control type="number" name="price" placeholder="Enter game price" style={{ width: '300px' }} />
+              </Form.Group>
 
-            <Form.Group className='mb-3'>
-              <Form.Label>Stock:</Form.Label>
-              <Form.Control type="number" name="stock" placeholder="Enter game stock" style={{ width: '300px' }} />
-            </Form.Group>
-            <Form.Group className='mb-3'>
-              <Form.Label>Console:</Form.Label>
-              <Form.Control as="select" name="console_id" style={{ width: '300px' }}>
-                {props.consoles.map(console => (
-                  <option key={console.id} value={console.id}>{console.name}</option>
-                ))}
-              </Form.Control>
-            </Form.Group>
-            <br></br>
-            <Button variant="primary" type="submit" className="formgamesbutton">
-              Create game
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
+              <Form.Group className='mb-3'>
+                <Form.Label>Stock:</Form.Label>
+                <Form.Control type="number" name="stock" placeholder="Enter game stock" style={{ width: '300px' }} />
+              </Form.Group>
+              <Form.Group className='mb-3'>
+                <Form.Label>Console:</Form.Label>
+                <Form.Control as="select" name="console_id" style={{ width: '300px' }}>
+                  {props.consoles.map(console => (
+                    <option key={console.id} value={console.id}>{console.name}</option>
+                  ))}
+                </Form.Control>
+              </Form.Group>
+              <br></br>
+              <Button variant="primary" type="submit" className="formgamesbutton">
+                Create game
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
+      </div>
     </div>
-
   );
 }
